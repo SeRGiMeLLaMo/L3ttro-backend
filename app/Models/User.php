bdb402b1394a8aebd4ce14cf4cb8a7424f75ab36
SeRGiMeLLaMo <sergiocube2003@gmail.com>
2025-11-27 19:15:10 +0100

    model & migration of User

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,12 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
     /**
@@ -19,8 +17,15 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'username',
+        'description',
         'email',
         'password',
+        'rol',
+        'photo',
+        'follows',
+        'stories',
+        'follow_button',
     ];
 
     /**
@@ -34,7 +39,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * The attributes that should be cast.
      *
      * @return array<string, string>
      */
@@ -42,6 +47,8 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
+            'stories' => 'boolean',
+            'follow_button' => 'boolean',
             'password' => 'hashed',
         ];
     }
