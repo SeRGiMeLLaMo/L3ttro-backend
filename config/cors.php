ba833fab436c3720a7246833c3f7af0945979eda
SeRGiMeLLaMo <sergiocube2003@gmail.com>
2026-05-11 18:10:30 +0200

    Fix: CORS and Sanctum stateful domains for Vercel deployment

--- config/cors.php
+++ config/cors.php
@@ -6,15 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        env('FRONTEND_URL', 'http://localhost:5173'),
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'http://localhost:5174',
-        'http://127.0.0.1:5174',
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
