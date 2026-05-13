eb602907cb6db81dc93c5cc0dc83f09e98d19cde
SeRGiMeLLaMo <sergiocube2003@gmail.com>
2026-05-13 17:58:13 +0200

    Fix: Hardened database config against malformed options string

--- config/database.php
+++ config/database.php
@@ -96,6 +96,7 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'require',
+            'options' => [],
         ],
 
         'sqlsrv' => [
